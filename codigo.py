ea463fd960895a0b1c159379c60be2cbd337045a
jaimeme15 <150305565+jaimeme15@users.noreply.github.com>
2023-11-23 18:03:41 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,14 +1,9 @@
 import time
-
-# Iniciar el temporizador
+# Start the timer
 start_time = time.time()
-
-# Bucle del 1 al 100,000
+# Loop from 1 to 100,000
 for number in range(1, 100001):
-    # Realizar alguna operación (en este caso, no se está haciendo nada)
-
-# Calcular el tiempo transcurrido
-elapsed_time = time.time() - start_time
-
-# Imprimir el tiempo total transcurrido
-print(f"Elapsed Time: {elapsed_time:.2f} seconds")
+ # Calculate the elapsed time
+ elapsed_time = time.time() - start_time
+ # Print the current number and the elapsed time
+ print(f"Number: {number}, Elapsed Time: {elapsed_time:.2f} seconds")
