9da6a4751da61edb2e632d7b8d38951b6ce1dc69
jaimeme15 <150305565+jaimeme15@users.noreply.github.com>
2023-11-23 18:01:46 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,7 +1,14 @@
-# Abrir un archivo en modo de escritura
-with open('numeros.txt', 'w') as archivo:
-    # Escribir los números del 1 al 100,000 en el archivo
-    for numero in range(1, 100001):
-        archivo.write(str(numero) + '\n')
+import time
 
-print("Archivo creado exitosamente.")
+# Iniciar el temporizador
+start_time = time.time()
+
+# Bucle del 1 al 100,000
+for number in range(1, 100001):
+    # Realizar alguna operación (en este caso, no se está haciendo nada)
+
+# Calcular el tiempo transcurrido
+elapsed_time = time.time() - start_time
+
+# Imprimir el tiempo total transcurrido
+print(f"Elapsed Time: {elapsed_time:.2f} seconds")
